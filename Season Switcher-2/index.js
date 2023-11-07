601470d8534458637087601084bcc6c8175f4daa
Boddapati Veeravenkata Vamsi <b.v.v.vamsi@gmail.com>
2023-11-07 23:18:58 +0530

    Seasons Switcher
    In this assignment, let's build a Seasons Switcher by applying the concepts we learned till now.
    
    Refer to the below image.
    
    seasons-switcher
    Instructions:
    
    The HTML img elements should have the ids seasonSmallImage, seasonMediumImage
    Warning
    Do not edit or remove the ids of the HTML button elements in the prefilled code.
    By following the above instructions, achieve the given functionality.
    
    When the HTML button element with the season id is clicked, the season images in both the small and medium devices should change respectively.
    Note
    Try to achieve the design as close as possible.
    Apply the functionality to the given prefilled code.
    Resources
    Use the image URLs given below.
    
    https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png
    https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png
    https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png
    https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png
    https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png
    https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png
    https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png
    https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png
    Concepts Review
    Want to quickly review some of the concepts you’ve been learning?
    
    Take a look at the Cheat Sheets.

---
+++ Season Switcher-2/index.js
@@ -0,0 +1,26 @@
+let smallimage = document.getElementById("smallImage")
+let bigimage = document.getElementById("bigImage")
+
+function button1() {
+    smallimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png"
+    bigimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png"
+
+}
+
+function button2() {
+    smallimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png"
+    bigimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png"
+
+}
+
+function button3() {
+    smallimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png"
+    bigimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png"
+
+}
+
+function button4() {
+    smallimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png"
+    bigimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png"
+
+}
